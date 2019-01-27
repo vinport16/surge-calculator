3349becd1a33af24abc4c8e5cc93ed8c490232b6
Vincent Portelli <vinport16@gmail.com>
2019-01-26 20:43:08 -0500

    added ccpts to text alert

--- index.js
+++ index.js
@@ -455,6 +455,7 @@ function notify_everyone(row){
       lines.push("Waiting: "+row.waiting);
       lines.push("Longest wait: "+minutes_to_time_string(row.waitTime));
       lines.push("ESI2 w/o bed: "+row.esi2noBed);
+      lines.push("Crit Care: "+row.critCarePatients);
       lines.push("ED is "+row.diversion);
       lines.push("Surge Score: "+row.surgeScore);
       lines.push("Notes: "+row.notes);
